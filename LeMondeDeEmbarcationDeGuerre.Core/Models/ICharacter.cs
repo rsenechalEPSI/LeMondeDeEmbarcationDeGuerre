94a2f8741938be391c292f5ce3854573cdd444e6
rsenechalEPSI <romain.senechal1@epsi.fr>
2021-10-21 14:30:36 +0200

    Ajoutez des fichiers projet.

---
+++ LeMondeDeEmbarcationDeGuerre.Core/Models/ICharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeMondeDeEmbarcationDeGuerre.Core.Models
+{
+    public interface ICharacter
+    {
+        public void Attack(Character characterToHit);
+        public void Heal(Character characterToHeal);
+
+        
+    }
+}
